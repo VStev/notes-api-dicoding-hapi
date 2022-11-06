493d1045d47b44b4e2000e6a78f3c046aa044b84
Aprilla <markusssandreas@gmail.com>
2022-11-06 10:58:35 +0700

    port fix

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   const server = new Hapi.Server({
-    port: 6969,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
